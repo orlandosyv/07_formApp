2eb5d2d11640dca1c51251440782a2f43f6b0af0
OrlandoYV <sergioyv@gmail.com>
2024-08-24 19:28:40 -0500

    oy - errors on validations

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -18,17 +18,38 @@ export class BasicPageComponent implements OnInit {
   //   inStorage: new FormControl(0)
   // });
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
     //this.myForm.reset(rtx5090)
   }
 
+  isValidField(field: string): boolean | null {
+    return (
+      this.myForm.controls[field].errors && this.myForm.controls[field].touched
+    );
+  }
+
+  getFieldError(field: string): string | null {
+    if (!this.myForm.controls[field]) return null;
+    const errors = this.myForm.controls[field].errors || {}
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'this field is required';
+        case 'minlength':
+          return `Min ${errors['minlength'].requiredLength} caracters.`;
+      }
+      console.log(key)
+    }
+    return 'hello'
+
+  };
+
   public myForm: FormGroup = this.fb.group({
     name: ['', [Validators.required, Validators.minLength(3)]],
     price: [0, [Validators.required, Validators.min(0)]],
     inStorage: [0, [Validators.required, Validators.min(0)]],
-
   });
 
   onSave(): void {
@@ -40,6 +61,5 @@ export class BasicPageComponent implements OnInit {
     console.log(this.myForm.value);
 
     // this.myForm.reset({price: 10, inStorage: 0});
-
   }
 }
