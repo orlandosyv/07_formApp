79c8429d28afb85bd144102321df946fd9fc4c9d
Orlando YV <sergioyv@gmail.com>
2024-09-15 23:13:23 -0500

    oy - add favorites

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './dynamic-page.component.html',
   styles: ``,
 })
 export class DynamicPageComponent {
-
   public myForm: FormGroup = this.fb.group({
     name: ['', [Validators.required, Validators.minLength(3)]],
     favoriteGames: this.fb.array([
@@ -15,19 +14,64 @@ export class DynamicPageComponent {
     ]),
   });
 
-  constructor(private fb: FormBuilder) { }
+  public newFavorite: FormControl = new FormControl("", Validators.required);
 
-    get favoriteGames() {
-      return this.myForm.get('favoriteGames') as FormArray;
+  constructor(private fb: FormBuilder) {}
+
+  get favoriteGames() {
+    return this.myForm.get('favoriteGames') as FormArray;
+  }
+
+  onDeleteSubmit() {
+
+  }
+
+
+  onAddToFavorites() {
+    if (this.newFavorite.invalid) return;
+    //console.log(this.newFavorite.value)
+    const newGame = this.newFavorite.value;
+    this.favoriteGames.push(this.fb.control(newGame, Validators.required));
+    this.newFavorite.reset();
+  }
+
+  onDeleteFavorite(index: number): void {
+    this.favoriteGames.removeAt(index);
+  }
+
+  onSubmit(): void {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
     }
 
-    onSubmit(): void {
-        if (this.myForm.invalid) {
-           this.myForm.markAllAsTouched();
-           return;
-        }
+    console.log(this.myForm.value);
+    this.myForm.reset();
+  }
+  isValidFieldInArray(formArray: FormArray, index: number) {
+    return formArray.controls[index].errors
+      && formArray.controls[index].touched
+  }
 
-        console.log(this.myForm.value);
-        this.myForm.reset();
+  isValidField(field: string): boolean | null {
+    return (
+      this.myForm.controls[field].errors && this.myForm.controls[field].touched
+    );
+  }
+
+  getFieldError(field: string): string | null {
+    if (!this.myForm.controls[field]) return null;
+    const errors = this.myForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'this field is required';
+        case 'minlength':
+          return `Min ${errors['minlength'].requiredLength} caracters.`;
+      }
+      console.log(key);
     }
+    return 'hello';
+  }
 }
