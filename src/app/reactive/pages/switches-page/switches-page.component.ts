804e5548d0bfb51b5de186ceca1d2b8326a10e7d
Orlando YV <sergioyv@gmail.com>
2024-10-08 20:42:04 -0500

    oy - validators formControlName

--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -3,22 +3,29 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './switches-page.component.html',
-  styles: ``
+  styles: ``,
 })
 export class SwitchesPageComponent {
-
   public myForm: FormGroup = this.fb.group({
     gender: ['M', Validators.required],
     wantNotifications: [true, Validators.required],
-    termsAndConditions: [false, Validators.requiredTrue]
-  })
+    termsAndConditions: [false, Validators.requiredTrue],
+  });
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder) {}
 
   onSave() {
     if (this.myForm.invalid) {
-      this.myForm.markAllAsTouched()
+      this.myForm.markAllAsTouched();
     }
+
+    console.log(this.myForm.value);
+
   }
 
+  isValidField(field: string): boolean | null {
+    return (
+      this.myForm.controls[field].errors && this.myForm.controls[field].touched
+    );
+  }
 }
