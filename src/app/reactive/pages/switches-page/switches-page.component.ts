21f06693e302b818b9db2404e6c4d0313057b0b9
Orlando YV <sergioyv@gmail.com>
2024-10-08 20:55:37 -0500

    oy  - de structuring

--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,25 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './switches-page.component.html',
   styles: ``,
 })
-export class SwitchesPageComponent {
+export class SwitchesPageComponent implements OnInit {
   public myForm: FormGroup = this.fb.group({
     gender: ['M', Validators.required],
     wantNotifications: [true, Validators.required],
     termsAndConditions: [false, Validators.requiredTrue],
   });
 
+  public person = {
+    gender: 'F',
+    wantNotifications: false
+  }
+
   constructor(private fb: FormBuilder) {}
+  ngOnInit(): void {
+    this.myForm.reset(this.person);
+  }
 
   onSave() {
     if (this.myForm.invalid) {
       this.myForm.markAllAsTouched();
     }
 
+    const { termsAndConditions, ...newPerson } = this.myForm.value;
+
+    this.person = newPerson;
     console.log(this.myForm.value);
+    console.log(this.person);
+
 
   }
 
