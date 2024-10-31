c5291afcf855888caac80aadb8b1fe5efbdda8ee
Orlando YV <sergioyv@gmail.com>
2024-10-30 22:48:17 -0500

    oy - validator regex

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,27 +1,28 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { cantBeStrider } from '../../../shared/validators/validators';
+import * as customValidator from '../../../shared/validators/validators';
 
 @Component({
   templateUrl: './register-page.component.html'
 })
 export class RegisterPageComponent {
+
   public myForm: FormGroup = this.fb.group({
-    name: ['', [Validators.required]],
-    email: ['', [Validators.required]],
-    username: ['', [Validators.required, cantBeStrider]],
+    name: ['', [Validators.required, Validators.pattern(customValidator.firstNameAndLastnamePattern)]],
+    email: ['', [Validators.required, Validators.email]],
+    username: ['', [Validators.required, customValidator.cantBeStrider]],
     password: ['', [Validators.required, Validators.minLength(6)]],
     password2: ['', [Validators.required]]
   });
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder) {  }
 
   isValidField(field: string) {
     //TODO obtener validacion desde un servicio
+
   }
 
   onSubmit() {
     this.myForm.markAllAsTouched();
   }
-
 }
--- src/app/shared/validators/validators.ts
+++ src/app/shared/validators/validators.ts
@@ -1,6 +1,7 @@
-import { FormControl } from "@angular/forms";
+import { FormControl, ValidationErrors } from "@angular/forms";
 
-export const cantBeStrider = (control: FormControl) => {
+export const firstNameAndLastnamePattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+export const cantBeStrider = (control: FormControl): ValidationErrors | null => {
 
   const value: string = control.value.trim().toLowerCase();
 
