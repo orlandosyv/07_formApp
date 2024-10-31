7550cc7aeb15acc301f0cfb2c924192164d028bd
Orlando YV <sergioyv@gmail.com>
2024-10-30 20:45:35 -0500

    oy - validator function

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { cantBeStrider } from '../../../shared/validators/validators';
 
 @Component({
   templateUrl: './register-page.component.html'
@@ -8,8 +9,8 @@ export class RegisterPageComponent {
   public myForm: FormGroup = this.fb.group({
     name: ['', [Validators.required]],
     email: ['', [Validators.required]],
-    username: ['', [Validators.required]],
-    password: ['', [Validators.required], Validators.minLength(6)],
+    username: ['', [Validators.required, cantBeStrider]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
     password2: ['', [Validators.required]]
   });
 
